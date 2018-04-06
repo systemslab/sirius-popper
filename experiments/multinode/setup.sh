6d063c5c83736bcbd650e0ed95851b331bb2d0f8
Ivo Jimenez <ivo.jimenez@gmail.com>
2018-04-05 18:25:50 -0700

    WIP for multi-node on chameleon

---
+++ experiments/multinode/setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+
+docker run --rm \
+  -e OS_AUTH_URL=$OS_AUTH_URL \
+  -e OS_TENANT_ID=$OS_TENANT_ID \
+  -e OS_TENANT_NAME=$OS_TENANT_NAME \
+  -e OS_PROJECT_NAME=$OS_PROJECT_NAME \
+  -e OS_USERNAME=$OS_USERNAME \
+  -e OS_PASSWORD=$OS_PASSWORD \
+  -e OS_KEYNAME=$OS_KEYNAME \
+  -v `pwd`/enos:/enos \
+  --workdir=/enos \
+  ivotron/enos:2.3.0 up
+
+# obtain list of IPs to create Ansible inventory
+scripts/create_inventory enos/current/env ansible/
+
+
+exit 0
